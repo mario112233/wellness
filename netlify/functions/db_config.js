4b8f5814253606df9cc1e250a8afc6554c6ae375
mario112233 <158332682+mario112233@users.noreply.github.com>
2025-11-28 18:04:28 +0000

    Upewnienie się co do konfiguracji SSL dla Supabase w db_config.js

--- netlify/functions/db_config.js
+++ netlify/functions/db_config.js
@@ -1,10 +1,12 @@
 // netlify/functions/db_config.js
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    // TEN BLOK SSL JEST KRYTYCZNY DLA SUPABASE W CHMURZE:
     ssl: {
-        rejectUnauthorized: false
+        rejectUnauthorized: false // Ignoruje sprawdzenie certyfikatu
     }
 });
 
